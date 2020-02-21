c56059f86a0b03633b092c255b1cb30c3e390afc
Nathan Kloer <nathaniel.b.kloer@gmail.com>
2020-02-21 14:25:03 -0500

    added review retrival funcationality for users and reviews, added formating for readibility in console.log

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-// $.ajax({
-//     url: "https://api.yelp.com/v3/businesses/north-india-restaurant-san-francisco/reviews",
-//     type: "GET",
-//     crossDomain: true,
-//     headers: {
-//         'Authorization': 'Bearer bGjwk70663lZrFXl3rZgMcX_wbvmt4wV2GWyyowrxwvCJOeBtgi_k-uEGZn3pT68E1gI-UtBltYheZ51QEi9oYCFp3FDEQAekFiHziYqMh0m8VGXvnxHS5sqCalOXnYx' 
-//     },
-// success: data => {
-//     console.log(data);
-// },
-// error: e => {
-//     console.log(e);
-// }
-// })
-
 'use strict';
 
 const yelp = require('yelp-fusion');
 
-// Place holder for Yelp Fusion's API Key. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
 const apiKey = 'bGjwk70663lZrFXl3rZgMcX_wbvmt4wV2GWyyowrxwvCJOeBtgi_k-uEGZn3pT68E1gI-UtBltYheZ51QEi9oYCFp3FDEQAekFiHziYqMh0m8VGXvnxHS5sqCalOXnYx';
 
 const searchRequest = {
@@ -31,36 +14,60 @@ const searchRequest = {
 const client = yelp.client(apiKey);
 
 
-client.search(searchRequest).then(response => {
+let checkCity = []
+let entry = []
+let finalArray = []
 
-  // all Rsults is the raw, pre-prettified json
-  const allResults = response.jsonBody.businesses;
 
-  // this is all the results, but in a more readable format
-  const prettyJson = JSON.stringify(allResults, null, 4);
+var promise = new Promise(function (resolve, reject) {
 
-  // top rated ice cream stores in alpheretta in order of rating
-  let alpherettaIceCream = allResults.filter(function (el) {
-    return el.location.city = "alpheretta"
+  client.search(searchRequest).then(response => {
+
+    // allResults containes all results that meet search req
+    const allResults = response.jsonBody.businesses;
+
+    //prettyJson is the prettifed version
+    const prettyJson = JSON.stringify(allResults, null, 4);
+
+    //checks the buisness's location according to location.city
+    let alpharettaIceCream = allResults.filter(function (el) {
+      return el.location.city = "alpharetta"
+    })
+
+    //loop to begin populating array with what we want
+    for (let i = 0; i < alpharettaIceCream.length; i++) {
+      checkCity = alpharettaIceCream[i].location.display_address[1]
+    
+    //checks the city in the display_address if buisness is actually in Alpharetta before pushing it,
+      if (checkCity.includes('Alpharetta,')) {
+        entry.push({
+          name: alpharettaIceCream[i].name,
+          alias: alpharettaIceCream[i].alias,
+          rating: alpharettaIceCream[i].rating,
+          address: alpharettaIceCream[i].location.display_address[0] + " " + alpharettaIceCream[i].location.city,
+          review: "",
+          user: ""
+        })
+      }
+    }
+    resolve()
   });
-
-  // top five of alpherettaIceCream
-  let topFive = alpherettaIceCream.slice(0, 5)
-
-  //getReviews gets all the reviews and displays them of the top5 icecream places
-  for (let i = 0; i < topFive.length; i++) {
-    getReviews(topFive[i].id)
-  }
-//   let topFiveReviews = topFive.forEach(reviews(topFive.alias))
-  console.log(topFive);
-}).catch(e => {
-  console.log(e);
-});
-
-function getReviews(storeId) {
-    client.reviews(storeId).then(response => {
-        console.log(response.jsonBody.reviews[0].text);
-      }).catch(e => {
-        console.log(e);
-      });
 }
+).then(function () {
+  //for loop runs through reviews, looking at the current entry's alias to get the user and the review
+  for (let i = 0; i < 5; i++) {
+    client.reviews(entry[i].alias).then(response => {
+      let storeReview = response.jsonBody.reviews[0];
+      entry[i].review = storeReview.text;
+      entry[i].user = storeReview.user.name;
+      finalArray.push(entry[i])
+      if (finalArray.length === 5) {
+        console.log(finalArray);
+      }
+    }, function (err) {
+      console.log(err)
+    })
+  }
+}
+
+)
